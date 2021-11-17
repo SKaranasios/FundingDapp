4618846243d9f2be2a18e2a14a69db6c19a861d1
Skaranasios <stathis.karanasios@outlook.com>
2021-11-17 21:40:20 +0200

    deployed contract / load balance

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React, { useEffect, useState, /*useState*/ } from "react";
 import Web3 from 'web3';
 import detectEthereumProvider from '@metamask/detect-provider'
+import { loadContract } from './utils/load-contract';
 
 
 function App() {
@@ -12,8 +13,12 @@ function App() {
   //   [variable , function]
   const[web3Api, setWeb3Api] =  useState({
     provider : null ,
-    web3 : null
+    web3 : null ,
+    //reference of contract
+    contract : null
   })
+
+  const [balance,setBalance]= useState(null)
 
 
   const[account,setAccount] = useState(null)
@@ -29,13 +34,16 @@ function App() {
       //console.log(window.web3)
       //console.log(window.ethereum)
       const provider = await detectEthereumProvider() 
+      const { contract } = await loadContract("Funding",provider)
 
       if(provider){
         //using request in button to connect for bettet ui
         //provider.request({method:"eth_requestAccounts"})
         setWeb3Api({
           web3: new Web3(provider),
-          provider
+          provider ,
+          //loading contract
+          contract
         })
       }
       else{
@@ -72,6 +80,19 @@ function App() {
     //if it's empty nothing to change
   } , [])
 
+  useEffect(()=>{
+      const loadBalance = async () => {
+        //in contract i have address with web3 i can make requests to the network
+      const { contract ,web3 } = web3Api
+      const balance = await web3.eth.getBalance(contract.address)
+      setBalance(balance)
+    }
+
+    //checking if we have a contract
+
+    web3Api.contract && loadBalance()
+  }, [web3Api])
+
 
   //need to use this function only when web3api is initialized
   useEffect(()=> {
@@ -90,31 +111,29 @@ function App() {
 
 
   return (
-    <>
+    
       <div className="faucet-wrapper">
         <div className="faucet">
           <span> 
             <strong>Account:</strong>
           </span>
-          <h1>
+          <h1>          
             {account ? account:
             <button className= "button is-normal"
             onClick={() =>
               web3Api.provider.request({method: "eth_requestAccounts"}
-            )}>
+            )}> 
               Connect Wallet
              </button>
             }
           </h1>
-
           <div className="balance-view is-size-2 mb-4">
-            Current Balance: <strong>10</strong> ETH
+            Current Balance: <strong>{balance}</strong> ETH
           </div>
           <button className="button is-link  mx-3 is-medium">Donate</button>
           <button className="button is-primary is-medium  ">Withdraw</button>
         </div>
       </div>
-    </>
   );
 }
 
--- src/utils/load-contract.js
+++ src/utils/load-contract.js
@@ -1,16 +1,24 @@
 import contract from "@truffle/contract"
 
-
-export const loadContract = async (name) =>{
+//utilize contract inside web application
+//name,provider as parameters
+export const loadContract = async (name,provider) =>{
     //accesing json in public folder
     //fetch api
     const response = await fetch(`/contracts/${name}.json`)
     const Artifact = await response.json()
     //will not create abstraction , need to trasnformate into object structure to interact with
     //@truffle-contract-npm
+    const _contract = contract(Artifact)
+    //get deployed contract
+    _contract.setProvider(provider)
+
+    //first need to deploy them to the network
+    const deployedContract = await _contract.deployed()
+
 
     return {
         //abstraction
-        contract : contract(Artifact)
+        contract : deployedContract
     }
 }
